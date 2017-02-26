d6e3d4d48529d974d9298c605a321bd523170572
Derek Lopes <DSWiiLOrd1o1@gmail.com>
2017-02-26 03:00:40 -0800

    Initial Commit

---
+++ ideaHub/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ideaHub
+//
+//  Created by Derek Lopes on 2/26/17.
+//  Copyright © 2017 San Jose State University. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
